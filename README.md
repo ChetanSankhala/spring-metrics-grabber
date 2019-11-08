9dc87ff73e32b02d6f2966d50534ed967f6f0439
chetan.sankhala <chetan.sankhala@ishisystems.com>
2019-11-09 01:07:59 +0530

    Added wild card support in metrics name search.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # spring-metrics-grabber
-spring-metrics-grabber for listing all metrics with value on single endpoint
+spring-metrics-grabber for listing all metrics with value on single endpoint '/metricx'
--- src/main/java/org/springframework/metrix/controller/MetricxEndpoint.java
+++ src/main/java/org/springframework/metrix/controller/MetricxEndpoint.java
@@ -2,14 +2,17 @@ package org.springframework.metrix.controller;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 
-@RestController("/metricx")
+@RestController
 public class MetricxEndpoint {
 
     private MeterRegistry registry;
@@ -18,22 +21,31 @@ public class MetricxEndpoint {
         this.registry = registry;
     }
 
-    @GetMapping
-    public Map<String, Double> metric() {
-        Map<String, Double> names = new LinkedHashMap<>();
-        collectNames(names, this.registry);
-        return names;
+    @GetMapping("/metricx")
+    public Map<String, Double> metricx() {
+        Map<String, Double> matricKeyValue = new LinkedHashMap<>();
+        collectMetrics(matricKeyValue, this.registry, (String) -> true);
+        return matricKeyValue;
     }
 
-    private void collectNames(Map<String, Double> names, MeterRegistry registry) {
+    private void collectMetrics(Map<String, Double> matricKeyValue, MeterRegistry registry, Predicate<String> filter) {
         if (registry instanceof CompositeMeterRegistry) {
             ((CompositeMeterRegistry) registry).getRegistries()
-                    .forEach((member) -> collectNames(names, member));
+                    .forEach((member) -> collectMetrics(matricKeyValue, member, filter));
         } else {
             registry.getMeters()
+                    .stream()
+                    .filter(meter -> filter.test(meter.getId().getName()))
                     .forEach(meter ->
-                        names.put(meter.getId().getName(), meter.measure().iterator().next().getValue())
+                        matricKeyValue.put(meter.getId().getName(), meter.measure().iterator().next().getValue())
                     );
         }
+    }
+
+    @GetMapping("/metricx/{metricPattern}")
+    public Map<String, Double> metricPattern(@PathVariable("metricPattern") String metricPattern) {
+        Map<String, Double> matricsKeyValue = new LinkedHashMap<>();
+        collectMetrics(matricsKeyValue, this.registry, metricName -> Pattern.matches(metricPattern, metricName));
+        return matricsKeyValue;
     }
 }
