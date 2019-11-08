50f69a3ac39b2327cf275900f3449838394dd551
chetan.sankhala <chetan.sankhala@ishisystems.com>
2019-11-08 20:29:00 +0530

    Initial Commit, Added listing of metric with value on /metrics endpoint

---
+++ src/main/java/com/springx/Application.java
@@ -0,0 +1,13 @@
+package com.springx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] appArguments) {
+        final SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.run(appArguments);
+    }
+}
